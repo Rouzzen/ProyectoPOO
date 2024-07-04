27a15dff0af1c1470ed107d5f81faceaaded1e80
dmoyan0 <diego.moyano@usm.cl>
2024-07-03 20:07:56 -0400

    Agregar puestos

--- views.py
+++ views.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-views = Blueprint(__name__, "views")
+views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
     return render_template("index.html")
+
+@views.route('/add', methods=['POST'])
+def add_puesto():
+    title = request.form.get('title')
+    price = request.form.get('price')
+    image_url = request.form.get('image_url')
+    return render_template('index.html', title=title, price=price, image_url=image_url)
