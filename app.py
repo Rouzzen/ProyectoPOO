2d772e713b9ab71e13f53e8eb9d979dab0535b31
Rouzzen <vicente.neira@usm.cl>
2024-07-07 23:36:38 -0400

    Creación de BDD

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from views import views
+from db import mysql
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "proyecto"
+
+mysql.init_app(app)
+
 app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
-
--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from db import mysql  # Importa mysql desde db.py
 
 views = Blueprint("views", __name__)
 
@@ -12,3 +13,19 @@ def add_puesto():
     price = request.form.get('price')
     image_url = request.form.get('image_url')
     return render_template('index.html', title=title, price=price, image_url=image_url)
+
+@views.route("/usuario", methods=['GET','POST'])
+def usuario():
+    if request.method == 'POST':
+        user = request.form['user']
+        clave = request.form['clave']
+        nombre = request.form['nombre']
+        wsp = request.form['wsp']
+        datos = request.form['datos bancarios']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO usuario (usuario, clave, nombre, wsp, datos) VALUES (%s, %s, %s, %s, %s)", (user, clave, nombre, wsp, datos))
+        mysql.connection.commit()
+        cur.close()
+        return "success"
+    return render_template("usuario.html")
